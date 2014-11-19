50d011c028f57f370272a1269b42198c94504a38
australiabrisbanemission <brisbanemission@outlook.com>
2014-11-19 13:43:29 +1000

    ?

--- ABM-Office-System-cpp/ABM-Office-System-cpp.cpp
+++ ABM-Office-System-cpp/ABM-Office-System-cpp.cpp
@@ -35,7 +35,7 @@ CABMOfficeSystemcppApp::CABMOfficeSystemcppApp()
 {
 	// TODO: replace application ID string below with unique ID string; recommended
 	// format for string is CompanyName.ProductName.SubProduct.VersionInformation
-	SetAppID(_T("ABM-Office-System-cpp.AppID.NoVersion"));
+	SetAppID(_T("ABM-Office-System-cpp.ABMOS.1"));
 
 	// TODO: add construction code here,
 	// Place all significant initialization in InitInstance
